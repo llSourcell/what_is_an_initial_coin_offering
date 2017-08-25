7b38303c1068605483a45d5b7ceccf9551ad3cee
Siraj <sirajravel@gmail.com>
2017-08-25 13:32:20 +0200

    Update GustavoCoinCrowdsale.sol

--- contracts/GustavoCoinCrowdsale.sol
+++ contracts/GustavoCoinCrowdsale.sol
@@ -1,18 +1,18 @@
 pragma solidity ^0.4.11;
 
-import './GustavoCoin.sol';
+import './DemoCoin.sol';
 import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
 
-contract GustavoCoinCrowdsale is Crowdsale {
+contract DemoCoinCrowdsale is Crowdsale {
 
-  function GustavoCoinCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) Crowdsale(_startBlock, _endBlock, _rate, _wallet) {
+  function DemoCoinCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) Crowdsale(_startBlock, _endBlock, _rate, _wallet) {
   }
 
   // creates the token to be sold.
   // override this method to have crowdsale of a specific MintableToken token.
   function createTokenContract() internal returns (MintableToken) {
-    return new GustavoCoin();
+    return new DemoCoin();
   }
 
 }
