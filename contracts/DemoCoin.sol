8aef0d3e4c3d390fc9d795d075769dd8044951e4
Siraj <sirajravel@gmail.com>
2017-08-25 13:31:23 +0200

    Update and rename GustavoCoin.sol to DemoCoin.sol

--- contracts/DemoCoin.sol
+++ contracts/DemoCoin.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.11;
 
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 
-contract GustavoCoin is MintableToken {
-  string public name = "GUSTAVO COIN";
-  string public symbol = "GUS";
+contract DemoCoin is MintableToken {
+  string public name = "DEMO COIN";
+  string public symbol = "DEM";
   uint256 public decimals = 18;
 }
